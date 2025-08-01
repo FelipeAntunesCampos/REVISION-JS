f358c3ea03b16b2262e9032e620dd3a6ef491636
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-08-01 15:34:42 -0300

    Feat: Adiciona novas etapas ao projeto

---
+++ decisao.js
@@ -0,0 +1,16 @@
+let temChaveDeFerro = true;
+let temMapa = false;
+let nivel = 7;
+let vida = 45;
+
+//1. Caverna das sombras -> precisa ter chave e mapa.
+let podeEntrarCaverna = temChaveDeFerro && temMapa;
+console.log("Pode entrar na caverna?", podeEntrarCaverna);
+
+//2. templo do Dragao -> nivel >= 5 OU tem a chave;
+let podeEntrarTemplo = nivel >= 5 || temChaveDeFerro;
+console.log("Pode entrar no templo?", podeEntrarTemplo);
+
+//3. Bosque da Recuperacao -> vida < 50 e não tem mapa;
+let podeEntrarBosque = vida < 50 && !temMapa;
+console.log("Pode entrar no bosque?", podeEntrarBosque);
